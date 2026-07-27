7b4b520eeecc148830ce4fd41807e3b6221e3035
jayharsule5121 <jayharsule5121@gmail.com>
2026-07-27 08:53:19 +0530

    fix gmail smtp

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -138,7 +138,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'jayharsule5121@gmail.com'
-EMAIL_HOST_PASSWORD = 'zkiq kxav twmh nmck'
+EMAIL_HOST_PASSWORD = 'qcii fcln areh efyl'
 
 
 STATIC_URL = '/static/'
